b7684219f08649c552dc2e28666b2c85901c7c43
shashilaheshan <ebayshashila@gmail.com>
2019-01-10 11:39:05 +0530

    Provide your Google Server Key and Googler App Sender ID to Config.ts file

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Config {
+	public wordpressApiUrl = 'http://demo.titaniumtemplates.com/wordpress/?json=2';
+}
+export const OneSignalAppId='Your App ID';
+export const senderId='Sender ID';
